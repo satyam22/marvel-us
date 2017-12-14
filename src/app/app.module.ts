0816b31f30e5755dd4c25504b72b7a3ac9eef78c
Satyam Bansal <satyamsgsits1994@gmail.com>
2017-12-14 18:01:45 +0530

    added server details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent }  from './app.component';
 import {HeroesComponent} from './components/heroes/heroes.component';
@@ -8,7 +9,7 @@ import {MessageComponent} from './components/messages/messages.component'
 import {HeroService} from './components/heroes/hero.service';
 import {MessageService} from './components/messages/message.service';
 @NgModule({
-  imports:      [ BrowserModule,FormsModule,NgbModule.forRoot()],
+  imports:      [ HttpClientModule,BrowserModule,FormsModule,NgbModule.forRoot()],
   declarations: [ AppComponent,HeroesComponent,MessageComponent],
   bootstrap:    [ AppComponent ],
   providers:[HeroService,MessageService]
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './heroes.interface';
 import { HeroService } from './hero.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
     selector: 'app-heroes',
     templateUrl: './heroes.component.html',
@@ -9,8 +10,9 @@ import { HeroService } from './hero.service';
 export class HeroesComponent implements OnInit {
     selectedHero: Hero;
     comment: string;
-    heroes: Hero[]
-    constructor(private heroService:HeroService) {
+    heroes: Hero[];
+    result: string[] = [];
+    constructor(private heroService: HeroService, private http: HttpClient) {
     }
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
@@ -18,14 +20,20 @@ export class HeroesComponent implements OnInit {
     postComment() {
         if (this.selectedHero && this.comment.length != 0) {
             this.selectedHero.comments.push(this.comment);
-  //          changeComment(this.selectedHero.id, this.comment);
+            //          changeComment(this.selectedHero.id, this.comment);
             this.comment = "";
         }
     }
     ngOnInit() {
-this.getHeroes();
+        this.getHeroes();
+        // this.http.get('http://localhost:3007/api/test').subscribe(data => {
+        //     this.result = data['result'];
+
+        // }
+        )
+
     }
-    getHeroes():void{
-        this.heroService.getHero().subscribe(heroes=>{this.heroes=heroes});
+    getHeroes(): void {
+        this.heroService.getHero().subscribe(heroes => { this.heroes = heroes });
     }
 }
